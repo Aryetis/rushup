5802f8b6280d58d7cb0e3a3557d555c3944c9742
HijackedBrain <aramirg@hotmail.fr>
2017-10-24 00:42:46 +0200

    refactoring

--- Assets/StandardAssets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/StandardAssets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -8,6 +8,7 @@ using Random = UnityEngine.Random;
 /*
  * Know bug :  _ moon jump
  *             _ can stop mid air cause movement dependent of input
+ *             _ reprise à zero de la momentum mid air après arrêt ? check if falling speed impacted
  * 
  */
 
@@ -17,7 +18,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
     [RequireComponent(typeof (AudioSource))]
     public class FirstPersonController : MonoBehaviour
     {
-        [SerializeField] private float m_JumpSpeed;
+        [SerializeField] private float m_jumpStrength;
         [SerializeField] private float m_StickToGroundForce;
         [SerializeField] private float m_GravityMultiplier;
         [SerializeField] private MouseLook m_MouseLook;
@@ -29,7 +30,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private enum PlayerState {running, jumping, walling};
 
         private Camera m_Camera;
-        private bool m_Jump;
+        private bool m_JumpRequest;
         private float m_YRotation;
         private Vector2 m_Input;
         private Vector3 m_MoveDir = Vector3.zero;
@@ -39,7 +40,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private Vector3 m_OriginalCameraPosition;
         private float m_StepCycle;
         private float m_NextStep;
-        private bool m_Jumping;
         private AudioSource m_AudioSource;
 
         [SerializeField] private float m_minSpeed;                   // Player will start running at m_minSpeed
@@ -59,7 +59,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private Vector3 m_previousNormaliedBaseVector;       //
         private Vector3 m_previousMoveVector;                //
         private Vector3 m_previousMoveDir;
-        private bool m_wallrunning;                          // is player wallrunning ?
         // Speed value is split as the following : speed = m_minSpeed + m_speedporcentage*normalizedSpeedVector + m_stackSpeedFactor*m_stackSpeedBonus
         private float m_stackSpeedFactor; // factor of bonus speed stack
         private float m_speedPorcentage;  // factor of vanilla speed 
@@ -74,22 +73,21 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_OriginalCameraPosition = m_Camera.transform.localPosition;
             m_StepCycle = 0f;
             m_NextStep = m_StepCycle/2f;
-            m_Jumping = false;
-            m_wallrunning = false;
             m_AudioSource = GetComponent<AudioSource>();
 			m_MouseLook.Init(transform , m_Camera.transform);
             m_SpeedOMeterText = GameObject.Find ("SpeedOMeter").GetComponent<UnityEngine.UI.Text>();
         }
 
-
         // Update is called once per frame
         private void Update()
         {
-            RotateView();
+            // set camera rotation matrix
+            m_MouseLook.LookRotation (transform, m_Camera.transform);
+
             // the jump state needs to read here to make sure it is not missed
-            if (!m_Jump && !m_Jumping)
+            if (!m_JumpRequest && playerState!=PlayerState.jumping)
             {
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                m_JumpRequest = CrossPlatformInputManager.GetButtonDown("Jump");
             }
 
             if (!m_PreviouslyGrounded && m_CharacterController.isGrounded)
@@ -99,20 +97,16 @@ namespace UnityStandardAssets.Characters.FirstPerson
                 stopJump();
                 playerState = PlayerState.running;
             }
-            if (!m_CharacterController.isGrounded && !m_Jumping && m_PreviouslyGrounded)
-            {
-                m_MoveDir.y = 0f;
-            }
 
             m_PreviouslyGrounded = m_CharacterController.isGrounded;
 
-
             /****** DEBUG DEV ZONE aka CHEAT ZONE******/
             if (Input.GetKeyDown (KeyCode.A))
+                // decrease monster stack speed
                 decreaseMonsterStackSpeed ();
-                // increase monster stack speed
 
             if (Input.GetKeyDown(KeyCode.E))
+                // increase monster stack speed
                 increaseMonsterStackSpeed();
         }
 
@@ -132,15 +126,23 @@ namespace UnityStandardAssets.Characters.FirstPerson
             // Actualize SpeedOMeter UI text
             m_SpeedOMeterText.text = speed + "m/s";
 
-			float speedInput;
-            GetInput(out speedInput); // <=> walkspeed || runspeed => determine basic length of direction vector
+            /*** DEDUCT DESIRED DIRECTION FROM INPUTS ***/
+            // Read input
+            float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+            float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+            bool accelerating = (horizontal == 0 && vertical == 0)? false : true;
+            // Link m_Input
+            m_Input = new Vector2(horizontal, vertical);
+
+            // normalize input if it exceeds 1 in combined length:
+            if (m_Input.sqrMagnitude > 1)
+                m_Input.Normalize();
 
             // always move along the camera forward as it is the direction that it being aimed at
             Vector3 desiredMove = transform.forward * m_Input.y + transform.right * m_Input.x;
 
-            // get a normal for the surface that is being touched to move along it
-
             /*** SET MoveDir.x & z ***/
+            // get a normal for the surface that is being touched to move along it
             RaycastHit hitInfoDown;
             Physics.SphereCast (transform.position, m_CharacterController.radius, Vector3.down, out hitInfoDown,
                 m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
@@ -149,31 +151,46 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_MoveDir.x = desiredMove.x;
             m_MoveDir.z = desiredMove.z;
 
+            Debug.Log(desiredMove);
+
             /*** SET MoveDir.y ***/
-            if (m_CharacterController.isGrounded)
+            switch (playerState)
             {
-                m_MoveDir.y = -m_StickToGroundForce; // emulate gravity, in case player is walking on a slanted floor
-                if (m_Jump)
+                case PlayerState.running:
                 {
-                    // TODO TWEAK IT SO WE  keep a 1,5 factor between standard jump and max speed jump
-                    m_MoveDir.y = m_JumpSpeed;										
-                    startJump ();
+                    m_MoveDir.y = -m_StickToGroundForce; // emulate gravity, in case player is walking on a slanted floor
+                    if (m_JumpRequest)
+                    {
+                        // TODO TWEAK IT SO WE  keep a 1,5 factor between standard jump and max speed jump
+                        m_MoveDir.y = m_jumpStrength; // Give impulse to the player
+                        startJump ();
+                    }
+                    break;
+                }
+                case PlayerState.jumping:
+                {
+                    RaycastHit hitInfoUp;
+                    if ( Physics.SphereCast (transform.position, m_CharacterController.radius, Vector3.up, out hitInfoUp,
+                        m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore) ) // player hit its head during a jump     
+                        m_MoveDir.y=0;
+
+                    m_MoveDir.x = m_previousMoveDir.x;
+                    m_MoveDir.z = m_previousMoveDir.z;
+                    m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
+                    break;
+                }
+                case PlayerState.walling:
+                {
+                    break;
                 }
             }
-            else
-            {
-                RaycastHit hitInfoUp;
-                if ( Physics.SphereCast (transform.position, m_CharacterController.radius, Vector3.up, out hitInfoUp,
-                    m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore) ) // player hit its head during a jump     
-                    m_MoveDir.y=0;
-
-                m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
-            }
                 
-            /*** APPLY FORCE (according to correct acceleration, deceleration factors) ***/
+                
+            /*** CREATE FORCE (according to correct acceleration, deceleration factors) ***/
             Vector3 m_normalizeBaseVector;
             Vector3 moveVector;
 
+            // Setting localAccelFactor and localDecelFactor
             float localAccelFactor=0.0f;
             float localDecelFactor=0.0f;
 
@@ -199,15 +216,18 @@ namespace UnityStandardAssets.Characters.FirstPerson
                 }
             }
 
-            if (speedInput != 0) // acceleration, player is "inputing"
+            // Calculating m_SpeedPorcentage
+            if (accelerating)
             {
+                Debug.Log("bouze de mamouth");
                 m_normalizeBaseVector = m_MoveDir * Time.fixedDeltaTime;  // <=> normalized direction vector 
                 m_speedPorcentage += localAccelFactor * Time.fixedDeltaTime;
                 if (m_speedPorcentage > 100)
                     m_speedPorcentage = 100;
             }
-            else                 // deceleration
+            else // decelerating
             {
+                Debug.Log("bite de chat");
                 m_normalizeBaseVector = m_previousNormaliedBaseVector; 
                 m_speedPorcentage -= localDecelFactor * Time.fixedDeltaTime;
                 if (m_speedPorcentage < 0)
@@ -218,58 +238,13 @@ namespace UnityStandardAssets.Characters.FirstPerson
                         ((m_speedPorcentage * (m_normalizeBaseVector) / 100) * (m_maxNominalSpeed - m_minSpeed)) + // Linear acceleration
                         m_stackSpeedFactor * m_normalizeBaseVector; // stack
 
-
-
-
-
-//            Vector3 m_normalizeBaseVector;
-//            Vector3 moveVector;
-//            bool isSpeedNotZero = speedInput != 0;
-//            float accelDecelFactor = isSpeedNotZero ? m_accelerationFactor : (isGrounded ? m_decelerationFactor : m_decelerationJumpFactor);
-//            if (isSpeedNotZero) // acceleration
-//            {
-//                m_normalizeBaseVector = m_MoveDir * Time.fixedDeltaTime;  // <=> normalized direction vector 
-//                m_speedPorcentage += accelDecelFacto+r * Time.fixedDeltaTime;
-//                if (m_speedPorcentage > 100)
-//                    m_speedPorcentage = 100;
-//            }
-//            else                 // deceleration
-//            {
-//                m_normalizeBaseVector = m_previousNormaliedBaseVector; 
-//                m_speedPorcentage -= accelDecelFactor * Time.fixedDeltaTime;
-//                if (m_speedPorcentage < 0)
-//                    m_speedPorcentage = 0;
-//            }
-//
-//            if (m_CharacterController.isGrounded)
-//            {
-//                if (m_speedPorcentage == 0)
-//                    moveVector = m_MoveDir;
-//                else // normal use case
-//                moveVector = m_minSpeed * m_normalizeBaseVector + // Minimal speed
-//                    ((m_speedPorcentage * (m_normalizeBaseVector) / 100) * (m_maxNominalSpeed - m_minSpeed)) + // Linear acceleration
-//                    m_stackSpeedFactor * m_normalizeBaseVector; // stack
-//
-//                // scotch
-//                if (isGrounded == false && moveVector.z == 0)
-//                {
-//                    moveVector.z = m_previousMoveVector.z * 0.999f;
-//                }
-//            }
-//            else
-//                moveVector = m_minSpeed * m_normalizeBaseVector; // Minimal speed
-                    
-
-
-
-
-
+            /*** APPLY FORCE ***/
             m_CollisionFlags = m_CharacterController.Move(moveVector);  
            
+            /*** SET VARIABLE FROM THE PAST ***/
             m_previousMoveDir = m_MoveDir;
             m_previousNormaliedBaseVector = m_normalizeBaseVector;
             m_previousMoveVector = moveVector;
-            /******************/
 
             m_MouseLook.UpdateCursorLock();
         }
@@ -283,7 +258,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void PlayFootStepAudio()
         {
-            // unused atm
+            // unused atm so who cares !
             if (!m_CharacterController.isGrounded)
             {
                 return;
@@ -298,47 +273,18 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_FootstepSounds[0] = m_AudioSource.clip;
         }
 
-        private void GetInput(out float speed)
-        {
-            // Read input
-            float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
-            float vertical = CrossPlatformInputManager.GetAxis("Vertical");
-            
-            // set the desired speed 
-            speed = (horizontal == 0 && vertical == 0)? 0 : m_minSpeed;
-
-            m_Input = new Vector2(horizontal, vertical);
-
-            // normalize input if it exceeds 1 in combined length:
-            if (m_Input.sqrMagnitude > 1)
-            {
-                m_Input.Normalize();
-            }
-        }
-
-
-        private void RotateView()
-        {
-            m_MouseLook.LookRotation (transform, m_Camera.transform);
-        }
-
-
         private void OnControllerColliderHit(ControllerColliderHit hit)
         {
             Rigidbody body = hit.collider.attachedRigidbody;
-            //dont move the rigidbody if the character is on top of it
+            // dont move the rigidbody if the character is on top of it
             if (m_CollisionFlags == CollisionFlags.Below)
-            {
                 return;
-            }
-
+            
             if (body == null || body.isKinematic)
-            {
                 return;
-            }
-            body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
-        }
 
+            body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse); // basic repulsion force for walls and stuff
+        }
 
         public void increaseMonsterStackSpeed()
         {
@@ -354,20 +300,13 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void startJump()
         {
-            Debug.Log ("StartJump");
+            m_JumpRequest = false;
             playerState = PlayerState.jumping;
-            m_Jump = false;
-            m_Jumping = true;
         }
 
         private void stopJump()
         {
-            if (m_Jumping || m_Jump)
-            {
-                Debug.Log ("StopJump");
-                m_Jump = false;
-                m_Jumping = false;
-            }
+            playerState = PlayerState.running;
         }
     }
 }
