ba07e60f4e364e64a015dbd0f05a0fa2a8f9bce8
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-10-24 11:39:44 +0200

    Add projectiles!
    --
    
     Thus spake the master programmer:
    
    ``When the program is being tested, it is too late to make design changes.''

---
+++ Assets/InternalAssets/Scripts/CustomTurret/CustomShootingSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomShootingSystem : ShootingSystem
+{
+
+    //Main turret controller Component
+    TurretController controller;
+
+    //Random Variables
+    bool reloading = false;
+    float time = 0;
+
+    [Header("Projectile")]
+    [Tooltip("Prefab to be used as ammo")]
+    [SerializeField]
+    private GameObject projectile;
+
+    [SerializeField]
+    private Transform _spawner;
+
+    //Get the component
+    void Start()
+    {
+        controller = this.GetComponent<TurretController>();
+    }
+
+    void Update()
+    {
+        //check FireDelay after fire
+        if (time <= fireDelay)
+            time += Time.deltaTime;
+    }
+
+    public override void Fire(Vector3 hitPoint, GameObject hitObject)
+    { //with hit effect
+        if (time > fireDelay)
+        {
+            fireMuzzle.Stop();
+            fireMuzzle.Play();
+            time = 0;
+            controller._Audio.Play_Fire();
+
+            GameObject bullet = Instantiate(projectile, _spawner.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().transform.Rotate(0, 0, 90);
+            bullet.GetComponent<Rigidbody>().AddForce(_spawner.transform.forward * 1800);
+        }
+    }
+}
